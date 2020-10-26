01ed653f96ff7efe478becf22b1c648fd92cf112
Vladimir Filipovic <vladofilipovic@hotmail.com>
2020-10-26 13:31:54 +0100

    popravio

--- predavanja/primeri-java/src/P010203ParadigmeZ01ObjektnoNzdNzsPokretanje.java
+++ predavanja/primeri-java/src/P010203ParadigmeZ01ObjektnoNzdNzsPokretanje.java
@@ -1,16 +1,17 @@
 /**
- * Програм (у виду конзолне апликације) за одређивање најмањег зајдничког делиоца за три броја.
- * Програм је реализован коришћењем објектно-орјентисане парадигме.
- * Програмски код је модуларно организован - реализовано je чување Јава кода у различитим датотекама.
- * Покретање програма је могуће само из датотеке која у себи садржи методу main.
+ * Програм (у виду конзолне апликације) за одређивање најмањег зајдничког
+ * делиоца за три броја. Програм је реализован коришћењем објектно-орјентисане
+ * парадигме. Програмски код је модуларно организован - реализовано je чување
+ * Јава кода у различитим датотекама. Покретање програма је могуће само из
+ * датотеке која у себи садржи методу main.
  */
 
-class CeoBroj {
+class CeoBrojNas {
     // вредност датог целог броја
     int vrednost;
 
     // конструктор
-    CeoBroj(int vrednostBroja) {
+    CeoBrojNas(int vrednostBroja) {
 
         vrednost = vrednostBroja;
     }
@@ -22,51 +23,51 @@ class CeoBroj {
     }
 
     // одређивање НЗД датог броја и другог целог броја
-    CeoBroj NZD(CeoBroj drugi) {
+    CeoBrojNas NZD(CeoBrojNas drugi) {
         int prvaVrednost = vrednost;
         int drugaVrednost = drugi.vrednost;
         while (true)
             if (prvaVrednost > drugaVrednost)
                 // први је већи
                 if (prvaVrednost % drugaVrednost == 0)
-                    return new CeoBroj(drugaVrednost);
+                    return new CeoBrojNas(drugaVrednost);
                 else
                     prvaVrednost %= drugaVrednost;
             else
-                // први је мањи или једнак
-                if (drugaVrednost % prvaVrednost == 0)
-                    return new CeoBroj(prvaVrednost);
-                else
-                    drugaVrednost %= prvaVrednost;
+            // први је мањи или једнак
+            if (drugaVrednost % prvaVrednost == 0)
+                return new CeoBrojNas(prvaVrednost);
+            else
+                drugaVrednost %= prvaVrednost;
     }
 
     // одређивање НЗД датог целог броја, другог броја и трећег броја
-    CeoBroj NZD(CeoBroj drugi, CeoBroj treci) {
+    CeoBrojNas NZD(CeoBrojNas drugi, CeoBrojNas treci) {
         return NZD(drugi).NZD(treci);
     }
 
     // одређивање НЗС датог броја и другог целог броја
-    CeoBroj NZS(CeoBroj drugi) {
-        CeoBroj nzd = NZD(drugi);
+    CeoBrojNas NZS(CeoBrojNas drugi) {
+        CeoBrojNas nzd = NZD(drugi);
         int nzs = (vrednost * drugi.vrednost) / nzd.vrednost;
-        return new CeoBroj(nzs);
+        return new CeoBrojNas(nzs);
     }
 
     // одређивање НЗС датог целог броја, другог броја и трећег броја
-    CeoBroj NZS(CeoBroj drugi, CeoBroj treci) {
+    CeoBrojNas NZS(CeoBrojNas drugi, CeoBrojNas treci) {
         return NZS(drugi).NZS(treci);
     }
 
 }
 
-class PokretanjeObjektnoNzdNzs {
+class PokretanjeObjektnoNzdNzsCeoBrojNas {
 
     // улазна тачка програма
     public static void main(String[] args) {
         // бројеви чији се НЗД тражи
-        CeoBroj prvi = new CeoBroj(48);
-        CeoBroj drugi = new CeoBroj(120);
-        CeoBroj treci = new CeoBroj(56);
+        CeoBrojNas prvi = new CeoBrojNas(48);
+        CeoBrojNas drugi = new CeoBrojNas(120);
+        CeoBrojNas treci = new CeoBrojNas(56);
 
         // приказ бројева чији се НЗД и НЗС тражи
         System.out.print("Први број је ");
@@ -77,15 +78,15 @@ class PokretanjeObjektnoNzdNzs {
         treci.prikaz();
 
         // одређивање НЗД за ова три броја
-        CeoBroj nzd = prvi.NZD(drugi, treci);
+        CeoBrojNas nzd = prvi.NZD(drugi, treci);
         // одређивање НЗС за ова три броја
-        //CeoBroj nzs = prvi.NZS(drugi, treci);
+        // CeoBroj nzs = prvi.NZS(drugi, treci);
 
         // приказ резултата
         System.out.print("НЗД ова три броја је ");
         nzd.prikaz();
-        //System.out.print("НЗС ова три броја је ");
-        //nzs.prikaz();
+        // System.out.print("НЗС ова три броја је ");
+        // nzs.prikaz();
     }
 
 }
